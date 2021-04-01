69e223aab207ee4d310ab01add3132e5823aac57
Phillip Mates <phillip.mates@nubank.com.br>
2021-04-01 19:27:26 +0200

    remove cur_dir from run file

--- run.sh
+++ run.sh
@@ -2,10 +2,8 @@
 
 set -euo pipefail
 
-cur_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
-
 service=$1
 default_branch=$2
 migrations_directory=$3
 
-cd $cur_dir/src/ordnungsamt; clojure -m core "$service" "$default_branch" "$migrations_directory"
+cd src/ordnungsamt; clojure -m core "$service" "$default_branch" "$migrations_directory"
