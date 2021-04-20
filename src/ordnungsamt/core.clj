26e80126aedfa3f82d7d55ac511c2bb1bca17da3
Phillip Mates <phillip.mates@nubank.com.br>
2021-04-20 19:42:56 +0200

    fix typo in NPE fix

--- src/ordnungsamt/core.clj
+++ src/ordnungsamt/core.clj
@@ -21,9 +21,10 @@
 
 (defn- print-process-output [{:keys [exit out err]}]
   (letfn [(print-lines [output]
-            (some->> (string/split output (re-pattern (System/lineSeparator)))
-                     (map println)
-                     doall))]
+            (when output
+              (->> (string/split output (re-pattern (System/lineSeparator)))
+                   (map println)
+                   doall)))]
     (print-lines out)
     (when-not (zero? exit)
       (print-lines err))))
