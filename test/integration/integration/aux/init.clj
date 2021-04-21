c9d1d6a7d8d7637049b9ef870070dff19dec45e9
Gabriel Giussi <gabriel.giussi@nubank.com.br>
2021-04-21 09:54:15 -0300

    mkdir

--- test/integration/integration/aux/init.clj
+++ test/integration/integration/aux/init.clj
@@ -41,7 +41,8 @@
   (fn []
     (let [repo-dir    (str base-dir repository)
           mock-client (client/new-client {:token-fn (constantly "token")})]
-      (run-commands! [["cp" "-r" (str "test-resources/" repository) repo-dir]
+      (run-commands! [["mkdir" "-p" repo-dir]
+                      ["cp" "-r" (str "test-resources/" repository) repo-dir]
                       ["git" "init" "." :dir repo-dir]
                       ["git" "add" "4'33" "clouds.md" "fanon.clj" :dir repo-dir]
                       ["git" "-c" "commit.gpgsign=false" "commit" "-m" "initial commit" :dir repo-dir]])
