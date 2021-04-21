429848eb4ef3b616a930de8090e4839b8965133d
Gabriel Giussi <gabriel.giussi@nubank.com.br>
2021-04-21 11:20:12 -0300

    test filter-opt-in

--- test/unit/ordnungsamt/core_test.clj
+++ test/unit/ordnungsamt/core_test.clj
@@ -1,3 +1,16 @@
 (ns ordnungsamt.core-test
   (:require [clojure.test :refer :all]
             [ordnungsamt.core :as core]))
+
+(deftest test-filter-opt-in
+  (let [filter (core/filter-opt-in "serviceA")]
+    (testing "passes if the service is in the opt-in"
+      (is (true? (filter {:opt-in #{"serviceA" "serviceB"}}))))
+
+    (testing "passes if opt-in is nil"
+      (is (true? (filter {}))))
+
+    (testing "filtered out if the service is not in the opt-in"
+      (is (false? (filter {:opt-in #{"serviceB"}}))))))
+
+
