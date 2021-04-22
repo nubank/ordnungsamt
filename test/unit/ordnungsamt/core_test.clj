f353e8b1929f241d7386bc168231ab6f660fa419
Gabriel Giussi <gabriel.giussi@nubank.com.br>
2021-04-22 07:30:05 -0300

    Opt-in migrations (#11)
    * Opt-in migrations
    
    * Remove comment
    
    * Fix tests?
    
    * mkdir
    
    * wtf
    
    * wtf
    
    * remove -r flag
    
    * add * to copy only files
    
    * maybe this time
    
    * cleanup
    
    * remove println
    
    * test filter-opt-in
    
    * remove comments
    
    * test compose-filters
    
    * Adding some tests to compose-filters
    
    * use one-liners assertions

--- test/unit/ordnungsamt/core_test.clj
+++ test/unit/ordnungsamt/core_test.clj
@@ -1,3 +1,28 @@
 (ns ordnungsamt.core-test
   (:require [clojure.test :refer :all]
             [ordnungsamt.core :as core]))
+
+(deftest test-filter-opt-in
+  (let [filter (core/filter-opt-in "serviceA")]
+    (testing "passes if the service is in the opt-in"
+      (is (true? (filter {:opt-in #{"serviceA" "serviceB"}}))))
+
+    (testing "passes if opt-in is nil"
+      (is (true? (filter {}))))
+
+    (testing "filtered out if the service is not in the opt-in"
+      (is (false? (filter {:opt-in #{"serviceB"}}))))))
+
+(deftest test-compose-filters
+  (is (true? ((core/compose-filters []) "a")))
+
+  (is (true? ((core/compose-filters [(partial = "a")]) "a")))
+
+  (is (true? ((core/compose-filters [(partial = "a") string?]) "a")))
+
+  (is (false? ((core/compose-filters [(partial = "a") int?]) "a")))
+
+  (is (false? ((core/compose-filters [(constantly false)
+                                      (fn [_] (throw (Exception. "this exception shouldn't happen")))])
+               "a"))))
+
