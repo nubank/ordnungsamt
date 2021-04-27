b9b9b3c24ea182c27d02fd422e19bd86f32cd2d2
Phillip Mates <phillip.mates@nubank.com.br>
2021-04-27 12:37:52 +0200

    add script to close all prs (#12)
    * add script to close all prs
    
    * try/catch for repos I don't have perms on

---
+++ src/ordnungsamt/ops/close_all_prs.clj
@@ -0,0 +1,15 @@
+(ns ordnungsamt.ops.close-all-prs
+  (:require [clj-github.httpkit-client :as github-client]
+            [ordnungsamt.close-open-prs :refer [close-open-prs!]]))
+
+(defn -main [& [org file-with-repositories]]
+  (let [github-client   (github-client/new-client {:token (System/getenv "GITHUB_TOKEN")})
+        repositories    (-> file-with-repositories
+                            slurp
+                            read-string)
+        close-for-repo! (fn [repo]
+                          (println "closing for" repo)
+                          (try (close-open-prs! github-client org (str repo))
+                               (catch Exception e
+                                 (println "failed for" repo))))]
+    (run! close-for-repo! repositories)))
