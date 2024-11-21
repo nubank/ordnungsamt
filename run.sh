9a44834be77930411a025eea9ca80a21e0b6c11a
André Ribeiro Camargo <andre.camargo@nubank.com.br>
2024-11-21 09:08:26 -0300

    git add --force files (#26)
    * git add --force files
    
    * Print errors to stderr instead of a file

--- run.sh
+++ run.sh
@@ -8,4 +8,4 @@ default_branch=$3
 service_directory=$4
 migrations_directory=$5
 
-clojure -m ordnungsamt.core "$org" "$service" "$default_branch" "$service_directory" "$migrations_directory"
+clojure --report stderr -m ordnungsamt.core "$org" "$service" "$default_branch" "$service_directory" "$migrations_directory"
