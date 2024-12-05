a4dfe568524ff6a0e4120e473431ffedbe5bc3c0
André Camargo <andre.camargo@nubank.com.br>
2024-12-05 16:43:55 -0300

    Bump clojure and avoid branch collisions

--- src/ordnungsamt/core.clj
+++ src/ordnungsamt/core.clj
@@ -194,7 +194,7 @@
       read-string))
 
 (defn load+run-migrations! [github-client org service default-branch repository-directory migrations-directory]
-  (let [target-branch (str "auto-refactor-" (utils/today))
+  (let [target-branch (str "auto-refactor-" (random-uuid))
         migrations    (read-migrations! migrations-directory)]
     (run-migrations! github-client org service default-branch target-branch repository-directory migrations)))
 
